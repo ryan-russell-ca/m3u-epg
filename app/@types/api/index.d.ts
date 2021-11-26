8677ce034d58115a8210c3d1a43ce52e22507625
Ryan Russell <ryan.russell@ssense.com>
2021-11-26 15:38:19 -0500

    Updates

--- app/@types/api/index.d.ts
+++ app/@types/api/index.d.ts
@@ -1,23 +1,7 @@
-import { ChannelGroupModel, ChannelInfoModel, ChannelOrderModel } from '../m3u';
+import { ChannelInfoModel } from '../m3u';
 
 export interface GetChannelsPayload {
   channels: ChannelInfoModel[];
-  page: number;
-  size: number;
-  search: string;
-  totalItems: number;
-}
-
-export interface GetPlaylistChannelsPayload {
-  groups: ChannelOrderModel[];
-  page: number;
-  size: number;
-  search: string;
-  totalItems: number;
-}
-
-export interface GetChannelGroupsPayload {
-  groups: ChannelGroupModel[];
   page: number;
   size: number;
   search: string;
